f35e83ca874d084d6333fdfd71971143055964c3
maruokamagi-create <maruoka.magi@gmail.com>
2026-08-22 17:50:45 +0900

    Add v0.10.11 version marker

---
+++ version-v111.js
@@ -0,0 +1,1 @@
+(()=>{window.MAGI_ACTIVE_VERSION='0.10.11';const mark=()=>{for(const el of document.querySelectorAll('.sectionHead span,.status')){el.textContent=el.textContent.replace(/v0\.10\.[0-9]+/g,'v0.10.11').replace(/v0\.9\.[0-9]+/g,'v0.10.11')}};const oldClear=clearAll;clearAll=function(){oldClear();mark()};mark();})();
