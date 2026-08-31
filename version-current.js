30b0f8052d9cdabc347247ddea4825a633a91f3a
maruokamagi-create <maruoka.magi@gmail.com>
2026-08-31 22:17:23 +0900

    Bump MAGI version to 0.10.81

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.80';
+window.MAGI_VERSION='0.10.81';
 window.MAGI_VERSION_DATE='2026-08-31';
-window.MAGI_VERSION_LABEL='Judgment-text consistency for conditional decisions';
+window.MAGI_VERSION_LABEL='Drive session reuse without repeated OAuth popup';
 window.MAGI_PROTOCOL_VERSION='1.0';
