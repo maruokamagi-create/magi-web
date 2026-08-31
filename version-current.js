66aa0c6613790d8709d518b1feeef135a9100aad
maruokamagi-create <maruoka.magi@gmail.com>
2026-08-31 21:22:07 +0900

    Bump MAGI version to 0.10.78

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.77';
+window.MAGI_VERSION='0.10.78';
 window.MAGI_VERSION_DATE='2026-08-31';
-window.MAGI_VERSION_LABEL='Deterministic seven-inning pitching aggregation';
+window.MAGI_VERSION_LABEL='Single-pitcher scope and evidence-safe workload review';
 window.MAGI_PROTOCOL_VERSION='1.0';
