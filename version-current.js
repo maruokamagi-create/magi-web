6f93b93f875a86e70190904b961efa0869067392
maruokamagi-create <maruoka.magi@gmail.com>
2026-08-31 22:43:55 +0900

    Bump version to 0.10.84

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.83';
+window.MAGI_VERSION='0.10.84';
 window.MAGI_VERSION_DATE='2026-08-31';
-window.MAGI_VERSION_LABEL='Separate folder reload from OAuth popup';
+window.MAGI_VERSION_LABEL='Restore standard Google OAuth browser flow';
 window.MAGI_PROTOCOL_VERSION='1.0';
