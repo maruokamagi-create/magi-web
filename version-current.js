62014a7a513d0c6ab4b8fabbbc62918f1b1025bf
maruokamagi-create <maruoka.magi@gmail.com>
2026-08-31 22:38:57 +0900

    Bump version to 0.10.83

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.82';
+window.MAGI_VERSION='0.10.83';
 window.MAGI_VERSION_DATE='2026-08-31';
-window.MAGI_VERSION_LABEL='Close iOS Chrome OAuth blank tab';
+window.MAGI_VERSION_LABEL='Separate folder reload from OAuth popup';
 window.MAGI_PROTOCOL_VERSION='1.0';
