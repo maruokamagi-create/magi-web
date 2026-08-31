0d9259a1bca088338f459a774f9f25de5e368f29
maruokamagi-create <maruoka.magi@gmail.com>
2026-08-31 22:25:35 +0900

    Bump MAGI version to 0.10.82

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.81';
+window.MAGI_VERSION='0.10.82';
 window.MAGI_VERSION_DATE='2026-08-31';
-window.MAGI_VERSION_LABEL='Drive session reuse without repeated OAuth popup';
+window.MAGI_VERSION_LABEL='Close iOS Chrome OAuth blank tab';
 window.MAGI_PROTOCOL_VERSION='1.0';
