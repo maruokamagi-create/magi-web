568f36354a7b9350c65c350f3c4cb3333bb5dbe1
maruokamagi-create <maruoka.magi@gmail.com>
2026-08-31 18:23:48 +0900

    Update public version to v0.10.76

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.75';
+window.MAGI_VERSION='0.10.76';
 window.MAGI_VERSION_DATE='2026-08-31';
-window.MAGI_VERSION_LABEL='Pitching evidence isolation and historical CSV recovery';
+window.MAGI_VERSION_LABEL='Calendar-grounded language and pitching evidence isolation';
 window.MAGI_PROTOCOL_VERSION='1.0';
