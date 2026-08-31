01e80d02bb175063a8c8d1aec23a11e0d42e3f01
maruokamagi-create <maruoka.magi@gmail.com>
2026-08-31 22:08:53 +0900

    Bump MAGI version to 0.10.80

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.79';
+window.MAGI_VERSION='0.10.80';
 window.MAGI_VERSION_DATE='2026-08-31';
-window.MAGI_VERSION_LABEL='Independent persona decisions for provisional closer use';
+window.MAGI_VERSION_LABEL='Judgment-text consistency for conditional decisions';
 window.MAGI_PROTOCOL_VERSION='1.0';
