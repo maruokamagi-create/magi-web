6829f3e997a72a40bf21593cf6afd716292bb853
maruokamagi-create <maruoka.magi@gmail.com>
2026-08-31 18:58:34 +0900

    Update public version to v0.10.77

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.76';
+window.MAGI_VERSION='0.10.77';
 window.MAGI_VERSION_DATE='2026-08-31';
-window.MAGI_VERSION_LABEL='Calendar-grounded language and pitching evidence isolation';
+window.MAGI_VERSION_LABEL='Deterministic seven-inning pitching aggregation';
 window.MAGI_PROTOCOL_VERSION='1.0';
