964e0f0f88c294e09b199b4d143d8a2b06ab2170
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 17:50:34 +0900

    Describe one-tap scanned PDF OCR release

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.11.1';
+window.MAGI_VERSION='0.11.2';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Restore the familiar indexed status badge';
+window.MAGI_VERSION_LABEL='Complete scanned PDF OCR in one tap';
 window.MAGI_PROTOCOL_VERSION='1.0';
