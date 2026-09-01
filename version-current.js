a3f4312040b9879c23690813cca28d180a74bbd9
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 12:02:14 +0900

    Describe v0.10.93 batting breakdowns

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.92';
+window.MAGI_VERSION='0.10.93';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Fix mixed batting totals and use print preview for PDF';
+window.MAGI_VERSION_LABEL='Add isolated batting-order and opponent breakdowns';
 window.MAGI_PROTOCOL_VERSION='1.0';
