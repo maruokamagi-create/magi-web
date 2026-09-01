8a5f0a2279d6013a8bfb31e9136241b0f0cb6687
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 16:52:04 +0900

    Describe recent practice-only report release

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.98';
+window.MAGI_VERSION='0.10.99';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Add verified opponent splits to pitching reports';
+window.MAGI_VERSION_LABEL='Add recent six-game practice-only batting and pitching reports';
 window.MAGI_PROTOCOL_VERSION='1.0';
