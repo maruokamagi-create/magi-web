051d4c650d601e25f483336dfa28271ee3064d6c
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 12:40:43 +0900

    Describe v0.10.95 detail-only totals

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.94';
+window.MAGI_VERSION='0.10.95';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Verify totals, correct RISP, and recalculate batting splits';
+window.MAGI_VERSION_LABEL='Rebuild total batting stats from batting detail only';
 window.MAGI_PROTOCOL_VERSION='1.0';
