42af8a3dc660859832092a6a79afbc00308d729f
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 10:42:05 +0900

    Describe v0.10.90 batting source rules

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.89';
+window.MAGI_VERSION='0.10.90';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Merge complete batting totals and repair iPhone PDFs';
+window.MAGI_VERSION_LABEL='Use dedicated RISP sheet and xlsm batting source';
 window.MAGI_PROTOCOL_VERSION='1.0';
