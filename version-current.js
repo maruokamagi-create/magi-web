bbc5bc3c48499beae78654bc55ddf4808d29945d
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 12:16:18 +0900

    Describe verified v0.10.94 calculations

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.93';
+window.MAGI_VERSION='0.10.94';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Add isolated batting-order and opponent breakdowns';
+window.MAGI_VERSION_LABEL='Verify totals, correct RISP, and recalculate batting splits';
 window.MAGI_PROTOCOL_VERSION='1.0';
