341770af37295c77b9282d35f08403029c60a190
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 11:38:22 +0900

    Describe v0.10.91 report preview

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.90';
+window.MAGI_VERSION='0.10.91';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Use dedicated RISP sheet and xlsm batting source';
+window.MAGI_VERSION_LABEL='Preview A4 report before optional PDF creation';
 window.MAGI_PROTOCOL_VERSION='1.0';
