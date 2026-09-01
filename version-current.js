762d102e01fbbd3c1306661fa538fb2b67fb5873
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 10:25:06 +0900

    Release MAGI Web v0.10.88

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.87';
+window.MAGI_VERSION='0.10.88';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Direct batting record lookup with distributable PDF';
+window.MAGI_VERSION_LABEL='Clarify Drive completion totals and file states';
 window.MAGI_PROTOCOL_VERSION='1.0';
