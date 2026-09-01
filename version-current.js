1a99532ff3a8f28084cbcf0330e1d9eee6923eb1
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 17:44:14 +0900

    Describe indexed badge label release

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.11.0';
+window.MAGI_VERSION='0.11.1';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Read PDF text and OCR images locally at no cost';
+window.MAGI_VERSION_LABEL='Restore the familiar indexed status badge';
 window.MAGI_PROTOCOL_VERSION='1.0';
