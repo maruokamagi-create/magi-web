ae2b34b0d9814174d349c807720669cd536b7651
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 22:17:07 +0900

    Update current version metadata

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.11.2';
+window.MAGI_VERSION='0.11.16';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Complete scanned PDF OCR in one tap';
+window.MAGI_VERSION_LABEL='Deterministic best order report';
 window.MAGI_PROTOCOL_VERSION='1.0';
