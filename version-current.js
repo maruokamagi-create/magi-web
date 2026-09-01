b20f3154958984baf2133f6c926dc523ff01ef71
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 10:33:26 +0900

    Release MAGI Web v0.10.89

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.88';
+window.MAGI_VERSION='0.10.89';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Clarify Drive completion totals and file states';
+window.MAGI_VERSION_LABEL='Merge complete batting totals and repair iPhone PDFs';
 window.MAGI_PROTOCOL_VERSION='1.0';
