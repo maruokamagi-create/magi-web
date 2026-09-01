4e2a32123d6057d3c37f57c753e237fb013c2c98
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 10:17:48 +0900

    Release MAGI Web v0.10.87

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.86';
+window.MAGI_VERSION='0.10.87';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Scope batting integrity checks to requested player and season';
+window.MAGI_VERSION_LABEL='Direct batting record lookup with distributable PDF';
 window.MAGI_PROTOCOL_VERSION='1.0';
