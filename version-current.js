9462a6b199493c63301f9899b788558340be5df5
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 17:35:44 +0900

    Describe free PDF and OCR release

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.99';
+window.MAGI_VERSION='0.11.0';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Add recent six-game practice-only batting and pitching reports';
+window.MAGI_VERSION_LABEL='Read PDF text and OCR images locally at no cost';
 window.MAGI_PROTOCOL_VERSION='1.0';
