88f58b0a3f662d7368ff0cffe3f6fcbd6ae51287
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 22:42:20 +0900

    Update version for lineup policy

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.11.16';
+window.MAGI_VERSION='0.11.17';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Deterministic best order report';
+window.MAGI_VERSION_LABEL='Team-role-aware lineup review';
 window.MAGI_PROTOCOL_VERSION='1.0';
