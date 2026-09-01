f1b7d0cada3364d49dc1bd5cd79be430cc36ea9a
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 13:52:00 +0900

    Describe v0.10.96 career semantics

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.95';
+window.MAGI_VERSION='0.10.96';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Rebuild total batting stats from batting detail only';
+window.MAGI_VERSION_LABEL='Separate all-season career totals from season records';
 window.MAGI_PROTOCOL_VERSION='1.0';
