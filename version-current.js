38e605812d3f13a3f2d463e33d63ce505c64c167
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 16:02:21 +0900

    Describe opponent pitching split release

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.97';
+window.MAGI_VERSION='0.10.98';
 window.MAGI_VERSION_DATE='2026-09-01';
-window.MAGI_VERSION_LABEL='Understand pitching intent and build verified career reports';
+window.MAGI_VERSION_LABEL='Add verified opponent splits to pitching reports';
 window.MAGI_PROTOCOL_VERSION='1.0';
