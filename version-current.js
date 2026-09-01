1b8d10bcf9d46587a92ef81b3812d675ddfcfc55
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-01 09:53:41 +0900

    Release MAGI Web v0.10.86

--- version-current.js
+++ version-current.js
@@ -1,4 +1,4 @@
-window.MAGI_VERSION='0.10.85';
-window.MAGI_VERSION_DATE='2026-08-31';
-window.MAGI_VERSION_LABEL='Add Google Drive connection progress indicator';
+window.MAGI_VERSION='0.10.86';
+window.MAGI_VERSION_DATE='2026-09-01';
+window.MAGI_VERSION_LABEL='Scope batting integrity checks to requested player and season';
 window.MAGI_PROTOCOL_VERSION='1.0';
