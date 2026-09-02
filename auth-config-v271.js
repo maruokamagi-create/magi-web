2f41505e9da458c1130f06587642cabc9ed68240
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-02 23:00:58 +0900

    Add Supabase authentication approval gate

---
+++ auth-config-v271.js
@@ -0,0 +1,5 @@
+window.MAGI_AUTH_CONFIG = Object.freeze({
+  supabaseUrl: 'https://stqekbjijufefrykksji.supabase.co',
+  publishableKey: 'sb_publishable_XpdBtz2Nhqo08-n8AyRjzw_InKlDdrI',
+  productionUrl: 'https://magi-web.vercel.app/'
+});
