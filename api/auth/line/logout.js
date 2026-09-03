7598b4c140b8dc094906492946e25e42625f5c76
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-03 20:35:34 +0900

    Fix LINE logout redirect

--- api/auth/line/logout.js
+++ api/auth/line/logout.js
@@ -1,7 +1,17 @@
 import { clearSessionCookie, sendJson } from './_line.js';
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return sendJson(res, 405, { ok: false, error: 'Method not allowed' });
+  if (req.method !== 'GET' && req.method !== 'POST') {
+    return sendJson(res, 405, { ok: false, error: 'Method not allowed' });
+  }
+
   res.setHeader('Set-Cookie', clearSessionCookie());
+
+  if (req.method === 'GET') {
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+
   return sendJson(res, 200, { ok: true });
 }
