2e6defd1ad124d52f1a37f4e9694724a63db5dfb
maruokamagi-create <maruoka.magi@gmail.com>
2026-09-03 18:07:09 +0900

    Add LINE Login start endpoint

---
+++ api/auth/line/start.js
@@ -0,0 +1,21 @@
+import { callbackUrl, lineConfigured, randomToken, redirect, sendJson, setOauthCookies } from './_line.js';
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') return sendJson(res, 405, { ok: false, error: 'Method not allowed' });
+  if (!lineConfigured()) return sendJson(res, 503, { ok: false, configured: false, error: 'LINE Login is not configured yet' });
+
+  const state = randomToken();
+  const nonce = randomToken();
+  setOauthCookies(res, state, nonce);
+
+  const params = new URLSearchParams({
+    response_type: 'code',
+    client_id: process.env.LINE_CHANNEL_ID,
+    redirect_uri: callbackUrl(),
+    state,
+    scope: 'openid profile',
+    nonce
+  });
+
+  return redirect(res, `https://access.line.me/oauth2/v2.1/authorize?${params.toString()}`);
+}
